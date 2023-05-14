e6fb2eb59de3b288922889970556e2abc48aa910
PecanPie <hidra0321@naver.com>
2023-05-15 01:19:00 +0900

    Added Extension for loading Only Carrot Images using Enum.

--- CDS_CarrotJob/CDS_CarrotJob/Utils/Extension/UIImage+Extension.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Utils/Extension/UIImage+Extension.swift
@@ -5,4 +5,38 @@
 //  Created by KYUBO A. SHIM on 2023/05/15.
 //
 
-import Foundation
+import UIKit.UIImage
+
+extension UIImage {
+    /// Only fetches Images of Carrot Assests
+    static func loadImageOf(carrotImageName image: CarrotImages) -> UIImage {
+        guard let imageToFetch = UIImage(named: image.rawValue) else {
+            print("Image Named \"\(image.rawValue)\" is not loaded. Pleas check the name.")
+            return UIImage()
+        }
+        
+        return imageToFetch
+    }
+}
+
+enum CarrotImages: String {
+    case carrotBell
+    case carrotCalendar
+    case carrotCalendarCheck
+    case carrotCalendarFill
+    case carrotChervonRight
+    case carrotClock
+    case carrotCoin
+    case carrotDeclaration
+    case carrotHeart
+    case carrotHeartFill
+    case carrotInformationCircle
+    case carrotNavigationBack
+    case carrotPencil
+    case carrotPersonProfile
+    case carrotRoleBox
+    case carrotShare
+    case carrotStar
+    case carrotTalkBubble
+    case carrotXMark
+}
