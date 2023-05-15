e31180b2e2eacc1a9c1c3bf2355d8879ec64060a
kwonjeong <kwonjeong9470@gmail.com>
2023-05-15 17:39:49 +0900

    [Setting] #5 - Color 에셋, enum 추가

---
+++ CDS_CarrotJob/CDS_CarrotJob/Utils/NameSpace/Color.swift
@@ -0,0 +1,32 @@
+//
+//  Color.swift
+//  CDS_CarrotJob
+//
+//  Created by KJ on 2023/05/15.
+//
+
+import UIKit
+
+public enum Color {
+
+    //MARK: - Sub Color
+    
+    public static let white = UIColor(named: "white")!
+    public static let gray7 = UIColor(named: "gray7")!
+    public static let gray6 = UIColor(named: "gray6")!
+    public static let gray5 = UIColor(named: "gray5")!
+    public static let gray4 = UIColor(named: "gray4")!
+    public static let gray3 = UIColor(named: "gray3")!
+    public static let gray2 = UIColor(named: "gray2")!
+    public static let gray1 = UIColor(named: "gray1")!
+    public static let blue = UIColor(named: "blue")!
+    public static let blue1 = UIColor(named: "blue1")!
+    public static let blue2 = UIColor(named: "blue2")!
+    public static let errorRed = UIColor(named: "errorRed")!
+    
+    //MARK: - Main Color
+    
+    public static let mainColor1 = UIColor(named: "mainColor1")!
+    public static let mainColor2 = UIColor(named: "mainColor2")!
+    public static let mainColor3 = UIColor(named: "mainColor2")!
+}
