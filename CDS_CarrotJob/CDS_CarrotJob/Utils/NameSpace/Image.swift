0587a001e4d3827b3b5216c4d041a874afc33a8a
kwonjeong <kwonjeong9470@gmail.com>
2023-05-15 18:17:33 +0900

    [Setting] #5 - Image enum 추가

---
+++ CDS_CarrotJob/CDS_CarrotJob/Utils/NameSpace/Image.swift
@@ -0,0 +1,33 @@
+//
+//  Image.swift
+//  CDS_CarrotJob
+//
+//  Created by KJ on 2023/05/15.
+//
+
+import UIKit
+
+public enum Image {
+
+    // MARK: - Image
+    
+    public static let carrotBell = UIImage(named: "carrotBell")!
+    public static let carrotCalendar = UIImage(named: "carrotCalendar")!
+    public static let carrotCalendarCheck = UIImage(named: "carrotCalendarCheck")!
+    public static let carrotCalendarFill = UIImage(named: "carrotCalendarFill")!
+    public static let carrotChervonRight = UIImage(named: "carrotChervonRight")!
+    public static let carrotClock = UIImage(named: "carrotClock")!
+    public static let carrotCoin = UIImage(named: "carrotCoin")!
+    public static let carrotDeclaration = UIImage(named: "carrotDeclaration")!
+    public static let carrotHeart = UIImage(named: "carrotHeart")!
+    public static let carrotHeartFill = UIImage(named: "carrotHeartFill")!
+    public static let carrotInformationCircle = UIImage(named: "carrotInformationCircle")!
+    public static let carrotNavigationBack = UIImage(named: "carrotNavigationBack")!
+    public static let carrotPersonProfile = UIImage(named: "carrotPersonProfile")!
+    public static let carrotPencil = UIImage(named: "carrotPencil")!
+    public static let carrotRoleBox = UIImage(named: "carrotRoleBox")!
+    public static let carrotShare = UIImage(named: "carrotShare")!
+    public static let carrotStar = UIImage(named: "carrotStar")!
+    public static let carrotTalkBubble = UIImage(named: "carrotTalkBubble")!
+    public static let carrotXMark = UIImage(named: "carrotXMark")!
+}
