8de2a6a89dc9434ed4892a8ed1ee52ca97416d85
PecanPie <hidra0321@naver.com>
2023-05-15 14:41:00 +0900

    [Setting] #3 - Added UIFont Extension for using NotoSans Font with weight and size

--- CDS_CarrotJob/CDS_CarrotJob/Utils/Extension/UIFont+Extension.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Utils/Extension/UIFont+Extension.swift
@@ -5,4 +5,24 @@
 //  Created by KYUBO A. SHIM on 2023/05/15.
 //
 
-import Foundation
+import UIKit.UIFont
+
+extension UIFont {
+    /// Returns NotoSans Font with its weight and size
+    static func notoSansFont(weightOf font: NotoSansFamily, sizeOf size: NotoSansFontSize) -> UIFont? {
+        return UIFont(name: "NotoSansKR-\(font.rawValue)", size: size.rawValue)
+    }
+    
+    enum NotoSansFamily: String {
+        case medium, bold
+    }
+    
+    enum NotoSansFontSize: CGFloat {
+        case font10 = 10
+        case font11
+        case font12
+        case font14 = 14
+        case font16 = 16
+        case font18 = 18
+    }
+}
