9bea8e4c1263316945d6e92b901a57d17f5ac613
kwonjeong <kwonjeong9470@gmail.com>
2023-05-16 18:08:31 +0900

    [Feat] #10 - Home Navigation View 생성

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Home/View/HomeNavigationView.swift
@@ -0,0 +1,51 @@
+//
+//  HomeNavigationView.swift
+//  CDS_CarrotJob
+//
+//  Created by KJ on 2023/05/16.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeNavigationView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension HomeNavigationView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
