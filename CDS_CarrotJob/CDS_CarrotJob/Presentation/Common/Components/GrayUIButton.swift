aa6594f0e79d07a8f076bd889cfa0056c232f2e8
PecanPie <hidra0321@naver.com>
2023-05-17 11:36:55 +0900

    Fixed color Asset

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Common/Components/GrayUIButton.swift
@@ -0,0 +1,20 @@
+//
+//  GrayUIButton.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/17.
+//
+
+import UIKit
+
+class GrayUIButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Common/Components/OrangeUIButton.swift
@@ -0,0 +1,20 @@
+//
+//  OrangeUIButton.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/17.
+//
+
+import UIKit
+
+class OrangeUIButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
