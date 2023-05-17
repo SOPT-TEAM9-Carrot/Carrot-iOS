b62ab50a02e6c572c9a90f461a0696f7761306e8
PecanPie <hidra0321@naver.com>
2023-05-17 11:37:19 +0900

    Added Common Button Components

--- CDS_CarrotJob/CDS_CarrotJob/Presentation/Common/Components/OrangeUIButton.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Common/Components/OrangeUIButton.swift
@@ -7,14 +7,66 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class OrangeUIButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    enum CarrotButtonType {
+        case inquireOnThePhone
+        case apply
+        case cancel
+        case leave
+        case confirm
+        
+        var title: String {
+            switch self {
+            case .inquireOnThePhone:
+                return "전화문의"
+            case .apply:
+                return "지원하기"
+            case .cancel:
+                return "취소"
+            case .leave:
+                return "나가기"
+            case .confirm:
+                return "확인"
+            }
+        }
     }
-    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    private func setUI() {
+        self.layer.cornerRadius = 5
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension OrangeUIButton {
+    func setUIOfButtonFor(type: CarrotButtonType) {
+        self.setTitle(type.title, for: .normal)
+        self.titleLabel?.font = .notoSansFont(weightOf: .Bold, sizeOf: .font14)
+        
+        switch type {
+        case .inquireOnThePhone:
+            self.setTitleColor(Color.mainColor1, for: .normal)
+            self.backgroundColor = Color.mainColor2
+        case .apply:
+            self.setTitleColor(.white, for: .normal)
+            self.backgroundColor = Color.mainColor1
+        case .cancel:
+            self.setTitleColor(Color.gray4, for: .normal)
+            self.backgroundColor = Color.gray7
+        case .leave, .confirm:
+            self.setTitleColor(.white, for: .normal)
+            self.backgroundColor = Color.mainColor1
+        }
+    }
 }
