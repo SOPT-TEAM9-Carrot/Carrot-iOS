2da70fc1ab062c09bb8087d8cabdc278d035d02e
kwonjeong <kwonjeong9470@gmail.com>
2023-05-18 21:06:40 +0900

    [Design] #17 - ReviewTableViewCell 구현

---
+++ CDS_CarrotJob/CDS_CarrotJob/Data/Model/ReviewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewModel.swift
+//  CDS_CarrotJob
+//
+//  Created by KJ on 2023/05/18.
+//
+
+import Foundation
--- CDS_CarrotJob/CDS_CarrotJob/Presentation/Review/Cell/ReviewTableViewCell.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Review/Cell/ReviewTableViewCell.swift
@@ -15,13 +15,14 @@ final class ReviewTableViewCell: UITableViewCell {
     // MARK: - UI Components
     
     private let profileImage = UIImageView()
-    private let userName = UILabel()
+    private let nickNameLabel = UILabel()
     private let actualWorkLabel = UILabel()
     private let certificationsCountLabel = UILabel()
     private let monthAgoLabel = UILabel()
-    private let reviewLabel = UILabel()
+    private let commentLabel = UILabel()
     private let jobLabel = UILabel()
     private let jobTitle = UILabel()
+    private let underLineView = UIView()
 
     // MARK: - View Life Cycle
     
@@ -48,7 +49,7 @@ extension ReviewTableViewCell {
             $0.image = Image.carrotPersonProfile
         }
         
-        userName.do {
+        nickNameLabel.do {
             $0.font = .notoSansFont(weightOf: .Bold, sizeOf: .font14)
             $0.textColor = Color.gray1
         }
@@ -56,6 +57,7 @@ extension ReviewTableViewCell {
         actualWorkLabel.do {
             $0.font = .notoSansFont(weightOf: .Bold, sizeOf: .font12)
             $0.textColor = Color.mainColor1
+            $0.text = "실제근무"
         }
         
         certificationsCountLabel.do {
@@ -68,7 +70,7 @@ extension ReviewTableViewCell {
             $0.textColor = Color.gray3
         }
         
-        reviewLabel.do {
+        commentLabel.do {
             $0.font = .notoSansFont(weightOf: .Medium, sizeOf: .font14)
             $0.textColor = Color.gray1
         }
@@ -87,12 +89,71 @@ extension ReviewTableViewCell {
             $0.font = .notoSansFont(weightOf: .Medium, sizeOf: .font12)
             $0.textColor = Color.gray3
         }
+        
+        underLineView.do {
+            $0.backgroundColor = Color.gray6
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
         
+        addSubviews(profileImage, nickNameLabel, actualWorkLabel,
+                    certificationsCountLabel, monthAgoLabel,
+                    commentLabel, jobLabel, jobTitle, underLineView)
+        
+        profileImage.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(16)
+        }
+        
+        nickNameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImage)
+            $0.leading.equalTo(profileImage.snp.trailing).offset(14)
+            $0.height.equalTo(20)
+        }
+        
+        actualWorkLabel.snp.makeConstraints {
+            $0.top.equalTo(nickNameLabel.snp.bottom)
+            $0.leading.equalTo(nickNameLabel)
+            $0.height.equalTo(20)
+        }
+        
+        certificationsCountLabel.snp.makeConstraints {
+            $0.top.equalTo(actualWorkLabel.snp.bottom)
+            $0.leading.equalTo(nickNameLabel)
+            $0.height.equalTo(20)
+        }
+        
+        monthAgoLabel.snp.makeConstraints {
+            $0.top.equalTo(certificationsCountLabel)
+            $0.leading.equalTo(certificationsCountLabel.snp.trailing).offset(15)
+            $0.height.equalTo(20)
+        }
+        
+        jobLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(17)
+            $0.leading.equalTo(nickNameLabel)
+            $0.width.equalTo(23)
+            $0.height.equalTo(15)
+        }
+        
+        jobTitle.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(16)
+            $0.leading.equalTo(jobLabel.snp.trailing).offset(4)
+            $0.height.equalTo(20)
+        }
+        
+        commentLabel.snp.makeConstraints {
+            $0.bottom.equalTo(jobTitle.snp.top)
+            $0.leading.equalTo(nickNameLabel)
+        }
+        
+        underLineView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(17)
+            $0.height.equalTo(1)
+        }
     }
     
     // MARK: - Methods
