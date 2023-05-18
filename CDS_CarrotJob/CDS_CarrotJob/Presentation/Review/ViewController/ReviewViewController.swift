33c35088caf197894e0333d76a6baed08df29dec
kwonjeong <kwonjeong9470@gmail.com>
2023-05-19 00:23:30 +0900

    [Feat] #22 - ReviewViewController 네비게이션 바 구현

--- CDS_CarrotJob/CDS_CarrotJob/Presentation/Review/ViewController/ReviewViewController.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Review/ViewController/ReviewViewController.swift
@@ -25,10 +25,15 @@ final class ReviewViewController: UIViewController {
     
     // MARK: - View Life Cycle
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
         setLayout()
+        setNavigationBar()
         setRegister()
         setDelegate()
     }
@@ -39,6 +44,8 @@ extension ReviewViewController {
     // MARK: - UI Components Property
     
     private func setUI() {
+        
+        view.backgroundColor = Color.white
         
         reviewTableView.do {
             $0.backgroundColor = .clear
@@ -69,7 +76,67 @@ extension ReviewViewController {
         reviewTableView.dataSource = self
     }
     
+    private func popToJobDetail() {
+        print("popToJobDetail")
+//        navigationController?.popViewController(animated: true)
+//        navigationController?.navigationBar.isHidden = true
+    }
+    
+//    private func setNavigationBar() {
+//
+//        navigationController?.navigationBar.tintColor = Color.gray1
+//        navigationController?.navigationBar.backgroundColor = Color.white
+//        navigationItem.leftBarButtonItem = UIBarButtonItem (
+//            image: Image.carrotXMark,
+//            style: .plain,
+//            target: self,
+//            action: #selector(backButtonTapped))
+//        let attributes: [NSAttributedString.Key: Any] = [
+//            NSAttributedString.Key.foregroundColor: Color.gray1,
+//            NSAttributedString.Key.font: UIFont.notoSansFont(weightOf: .Bold, sizeOf: .font14)!
+//        ]
+//        let navigationItem = UINavigationItem()
+//        let title = "후기"
+//        navigationItem.title = title
+//        navigationItem.titleView = UILabel()
+//
+//        if let titleLabel = navigationItem.titleView as? UILabel {
+//            titleLabel.attributedText = NSAttributedString(string: title, attributes: attributes)
+//        }
+//    }
+    
+    private func setNavigationBar() {
+        navigationController?.navigationBar.tintColor = Color.gray1
+        navigationController?.navigationBar.backgroundColor = Color.white
+        let buttonInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8) // 원하는 여백 값으로 수정
+        navigationItem.leftBarButtonItem = UIBarButtonItem (
+            image: Image.carrotNavigationBack,
+            style: .plain,
+            target: self,
+            action: #selector(backButtonTapped)
+        )
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.foregroundColor: Color.gray1,
+            NSAttributedString.Key.font: UIFont.notoSansFont(weightOf: .Bold, sizeOf: .font14)!
+        ]
+        let title = "후기"
+        navigationItem.title = title
+        
+        if let titleLabel = navigationItem.titleView as? UILabel {
+            titleLabel.attributedText = NSAttributedString(string: title, attributes: attributes)
+        } else {
+            let titleLabel = UILabel()
+            titleLabel.attributedText = NSAttributedString(string: title, attributes: attributes)
+            navigationItem.titleView = titleLabel
+        }
+    }
+
+    
     // MARK: - @objc Methods
+    
+    @objc private func backButtonTapped() {
+        popToJobDetail()
+    }
 }
 
 extension ReviewViewController: UITableViewDelegate {
@@ -77,7 +144,6 @@ extension ReviewViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
-    
 }
 
 extension ReviewViewController: UITableViewDataSource {
