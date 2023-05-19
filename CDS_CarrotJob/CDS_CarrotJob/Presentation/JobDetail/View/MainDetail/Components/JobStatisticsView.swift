6006dd3aba1f93eb29b7096a22fc1b50ca1e30c8
PecanPie <hidra0321@naver.com>
2023-05-19 17:48:06 +0900

    Added Files and new Image Assets

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/MainDetail/Components/JobStatisticsView.swift
@@ -0,0 +1,20 @@
+//
+//  JobStatisticsView.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/19.
+//
+
+import UIKit
+
+class JobStatisticsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/MainDetail/Components/LabelWithImage.swift
@@ -0,0 +1,20 @@
+//
+//  LabelWithImage.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/19.
+//
+
+import UIKit
+
+class LabelWithImage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
