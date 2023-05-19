1b4164ac2a035c91a62a212d7a2421928491b8a1
PecanPie <hidra0321@naver.com>
2023-05-19 18:07:51 +0900

    Added Files

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/DetailProfile/Components/KaKaoMapView.swift
@@ -0,0 +1,20 @@
+//
+//  KaKaoMapView.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/19.
+//
+
+import UIKit
+
+class KaKaoMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/DetailProfile/DetailProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailProfileView.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/19.
+//
+
+import UIKit
+
+class DetailProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
