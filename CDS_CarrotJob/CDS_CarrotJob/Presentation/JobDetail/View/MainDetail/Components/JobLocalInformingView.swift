52524ffe16264cf5eb54ead1642f34540c04d66a
PecanPie <hidra0321@naver.com>
2023-05-19 17:53:51 +0900

    Added component UIViews Files

--- CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/MainDetail/Components/JobLocalInformingView.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/MainDetail/Components/JobLocalInformingView.swift
@@ -7,14 +7,91 @@
 
 import UIKit
 
-class JobLocalInformingView: UIView {
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class JobLocalInformingView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let alarmImageView = UIImageView()
+    private let mainLabel = UILabel()
+    private let subLabel = UILabel()
+    private let chevronImageView = UIImageView()
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
     }
-    */
-
+    
+    // MARK: - Draw Life Cycle
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = 10
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+ 
+extension JobLocalInformingView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        self.backgroundColor = Color.mainColor3
+        
+        alarmImageView.do {
+            $0.image = UIImage.loadImageOf(carrotImageName: .carrotBell)
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        mainLabel.do {
+            $0.text = "OOO동 알바 찾으세요?"
+            $0.textColor = Color.gray1
+            $0.font = .notoSansFont(weightOf: .Bold, sizeOf: .font12)
+        }
+        
+        subLabel.do {
+            $0.text = "이 공고와 비슷한 공고가 새로 올라오면 알려드려요."
+            $0.textColor = Color.gray1
+            $0.font = .notoSansFont(weightOf: .Medium, sizeOf: .font12)
+            $0.numberOfLines = 1
+        }
+        
+        chevronImageView.do {
+            $0.image = UIImage.loadImageOf(carrotImageName: .carrotChervonRight)
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        self.addSubviews(alarmImageView, mainLabel, subLabel, chevronImageView)
+        
+        alarmImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(11)
+            $0.centerY.equalToSuperview()
+        }
+        
+        mainLabel.snp.makeConstraints {
+            $0.leading.equalTo(alarmImageView.snp.trailing).offset(8)
+            $0.top.equalToSuperview().inset(12)
+        }
+        
+        subLabel.snp.makeConstraints {
+            $0.leading.equalTo(mainLabel.snp.leading)
+            $0.bottom.equalToSuperview().inset(12)
+        }
+        
+        chevronImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+    }
 }
--- CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/MainDetail/Components/JobStatisticsView.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/MainDetail/Components/JobStatisticsView.swift
@@ -7,14 +7,58 @@
 
 import UIKit
 
-class JobStatisticsView: UIView {
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class JobStatisticsView: UIView {
+    
+    // MARK: - Properties
+    
+    private var applicantsCount: Int
+    private var likeCount: Int
+    private var clickedCount: Int
+    
+    // MARK: - UI Components
+    
+    private let mainLabel = UILabel()
+    
+    // MARK: - Initializer
+    
+    init(applicantsCount: Int, likeCount: Int, clickedCount: Int) {
+        self.applicantsCount = applicantsCount
+        self.likeCount = likeCount
+        self.clickedCount = clickedCount
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension JobStatisticsView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        mainLabel.do {
+            $0.text = "지원자 \(self.applicantsCount) · 관심 \(self.likeCount) · 조회 \(self.clickedCount)"
+            $0.textColor = Color.gray3
+            $0.font = .notoSansFont(weightOf: .Medium, sizeOf: .font11)
+            $0.textAlignment = .left
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        self.addSubview(mainLabel)
+        
+        mainLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.top.equalToSuperview()
+        }
+    }
 }
