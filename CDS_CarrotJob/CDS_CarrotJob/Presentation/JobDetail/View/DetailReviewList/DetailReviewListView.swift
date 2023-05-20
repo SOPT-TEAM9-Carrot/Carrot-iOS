9fe0d5ddae59a9a093798409ca91c8e1a05e9086
PecanPie <hidra0321@naver.com>
2023-05-20 15:38:14 +0900

    Added a fike

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/DetailReviewList/DetailReviewListView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailReviewListView.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/20.
+//
+
+import UIKit
+
+class DetailReviewListView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
