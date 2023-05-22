798f517c1afe875debd12c325a7411bb18f7f229
PecanPie <hidra0321@naver.com>
2023-05-22 13:22:52 +0900

    Modified methods name as Merged file changed the names

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/DetailReviewList/Components/DetailReviewPagingView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailReviewPagingView.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/20.
+//
+
+import UIKit
+
+class DetailReviewPagingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/DetailReviewList/Components/DetailReviewSegmentView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailReviewSegmentView.swift
+//  CDS_CarrotJob
+//
+//  Created by KYUBO A. SHIM on 2023/05/20.
+//
+
+import UIKit
+
+class DetailReviewSegmentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
