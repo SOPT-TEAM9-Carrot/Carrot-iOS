b7fec00b1aa2a0422f55437e678fafea8a3ab33b
kwonjeong <kwonjeong9470@gmail.com>
2023-05-22 20:27:22 +0900

    [Feat] HomeNearJobView 생성

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Home/View/Near/HomeNearJobView.swift
@@ -0,0 +1,51 @@
+//
+//  HomeNearJobView.swift
+//  CDS_CarrotJob
+//
+//  Created by KJ on 2023/05/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeNearJobView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension HomeNearJobView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
