84e52b837689bec6404b0a59a43797015a4e7138
kwonjeong <kwonjeong9470@gmail.com>
2023-05-24 15:36:57 +0900

    [Fix] #32 - CollectionView 익스텐션 수정

---
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/Home/View/Near/Cell/TodayPopularCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TodayPopularCollectionViewCell.swift
+//  CDS_CarrotJob
+//
+//  Created by KJ on 2023/05/23.
+//
+
+import UIKit
+
+class TodayPopularCollectionViewCell: UICollectionViewCell {
+    
+}
