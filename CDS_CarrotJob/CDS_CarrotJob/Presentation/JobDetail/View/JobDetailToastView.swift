19d58a207843835804fc632e7c2168032d76804f
PecanPie <hidra0321@naver.com>
2023-05-25 14:13:21 +0900

    [Feat] #33 - Added Animation to tapping Temp. and Coopy Button

--- CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/JobDetailToastView.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Presentation/JobDetail/View/JobDetailToastView.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 
-class JobDetailToastView: UIView {
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class JobDetailToastView: UIView {
+
+    private let toastLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = Color.gray2
+        
+        toastLabel.do {
+            $0.text = "주소가 복사되었습니다."
+            $0.font = .notoSansFont(weightOf: .Medium, sizeOf: .font11)
+            $0.textColor = .white
+        }
+        
+        self.addSubview(toastLabel)
+        
+        toastLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(12)
+        }
     }
-    */
-
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = 5
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
