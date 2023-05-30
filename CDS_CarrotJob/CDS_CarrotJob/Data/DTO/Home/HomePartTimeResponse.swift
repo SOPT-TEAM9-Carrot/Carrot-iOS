846d111dd0fb6b2976bac749a2ce835bc7cf53e7
kwonjeong <kwonjeong9470@gmail.com>
2023-05-30 23:57:38 +0900

    [Comment] #54 - 주석 제거

--- CDS_CarrotJob/CDS_CarrotJob/Data/DTO/Home/HomePartTimeResponse.swift
+++ CDS_CarrotJob/CDS_CarrotJob/Data/DTO/Home/HomePartTimeResponse.swift
@@ -5,11 +5,6 @@
 //  Created by KJ on 2023/05/25.
 //
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let contentResponse = try? JSONDecoder().decode(ContentResponse.self, from: jsonData)
-
 import Foundation
 
 // MARK: - HomeNearJobResponse
